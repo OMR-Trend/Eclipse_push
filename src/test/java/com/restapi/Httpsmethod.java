e63e0bc8ce22870fd61832268e489d361923346e
OMR-Trend <nitheesgopi@gmail.com>
2024-12-13 17:23:26 +0530

    First push

---
+++ src/test/java/com/restapi/Httpsmethod.java
@@ -0,0 +1,22 @@
+package com.restapi;
+
+import io.restassured.RestAssured; 
+
+import  static io.restassured.RestAssured.given;
+
+import org.apache.http.impl.client.TargetAuthenticationStrategy;
+
+public class Httpsmethod {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
